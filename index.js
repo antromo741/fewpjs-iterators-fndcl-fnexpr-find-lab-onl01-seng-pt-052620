774ffbe1e6016fb3710d6f0c0a447666e5bcbf80
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-01-03 19:41:38 -0500

    commit tree 58d0d8511ae2ac6f2ffed8429ce4e0093aeb225e

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+//looking in test indexTest they use record ass the array being passed so we call find on record 
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
 }
